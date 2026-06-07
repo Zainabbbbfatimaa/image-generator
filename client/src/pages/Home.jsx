65fa27f9bdc9d3d583f01474ed2bb621e40e9d09
Zainab Fatima <zainab.fatima8485@gmail.com>
2026-06-07 22:16:36 +0500

    Zainab complete generate button section

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -3,6 +3,7 @@ import Header from '../components/Header'
 import Steps from '../components/Steps'
 import Description from '../components/Description'
 import Testmonials from '../components/Testmonials'
+import GenerateBtn from '../components/GenerateBtn'
 
 const Home = () => {
   return (
@@ -11,6 +12,7 @@ const Home = () => {
         <Steps/>
         <Description/>
         <Testmonials/>
+        <GenerateBtn/>
 
     </div>
   )
